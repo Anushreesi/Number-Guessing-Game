b130f5c94a2186cc3a921d70351f1f40a897f5dc
Anushree Singh <anushreesingh1102@gmail.com>
2023-05-28 21:01:01 +0530

    changes

--- demo.py
+++ demo.py
@@ -1,16 +1,9 @@
 import random
 
 num = random.randint(0, 10)
-print(f'''
----------------------------
-Correct answer: {num}
----------------------------
-''')
-
 attempt = 4
 msg = ''
 while attempt > 0:
-   
     user_input = int(input('Enter the Number: '))
     if user_input == num:
         msg = 'You Won the round!'
@@ -18,14 +11,11 @@ while attempt > 0:
     elif user_input > num:
         print(f'{user_input} is greater.\nRemaining attempts are: {attempt}.')
         attempt -= 1
-        
-
     elif user_input < num:
         print(f'{user_input} is smaller.\nRemaining attempts are: {attempt}.')
         attempt -= 1
 
     else:
         print('Something went wrong!')
-        break
-    
+        break   
 print(msg)
